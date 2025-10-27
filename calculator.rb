66f970073b5009e542fe5dbc5cb24e05c7222f48
Swapnil Sable <swapnilsable.pro@gmail.com>
2025-10-27 16:50:27 +0530

    Add custom delimiter support

--- calculator.rb
+++ calculator.rb
@@ -6,6 +6,12 @@ class Calculator
   end
 
   def self.parsed_numbers(numbers)
-    numbers.split(/[,\n]/).map(&:to_i)
+    delimiter = ",|\n"
+    if numbers.start_with?("//")
+      delimiter_spec, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.escape(delimiter_spec[2..])
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i)
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe Calculator do
     it "handles multiple delimiters" do
       expect(Calculator.add("1\n2,3")).to eq(6)
     end
+
+    it "handles custom delimiters" do
+      expect(Calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
