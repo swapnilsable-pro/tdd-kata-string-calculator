4d9ed6336f4945d5e7fb063fc3055377739cdb98
Swapnil Sable <swapnilsable.pro@gmail.com>
2025-10-27 16:24:35 +0530

    Handle single number input

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Calculator do
     it "returns 0 for an empty string" do
       expect(Calculator.add("")).to eq(0)
     end
+
+    it "returns the number for a single number string" do
+      expect(Calculator.add("5")).to eq(5)
+    end
   end
 end
